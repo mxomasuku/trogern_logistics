1761cafc61a09ce651e874408d9977316a1c629b
Mxolisi Masuku <mxolisi.b.masuku@gmail.com>
2025-08-10 00:03:26 +0200

    fix: routing after logging in now working

--- dashboard-api/src/routes/auth.routes.ts
+++ dashboard-api/src/routes/auth.routes.ts
@@ -1,11 +1,12 @@
 import {Router} from 'express';
-import { loginUser, logoutUser,  } from '../controllers/auth.controller';
+import { loginUser, logoutUser, me  } from '../controllers/auth.controller';
 import { verifySessionCookie } from '../middleware/verifySessionCookie';
 import { loginLimiter } from '../middleware/rateLimit';
 
 const router = Router()
 
 router.post('/login', loginLimiter, loginUser )
+router.get('/me', verifySessionCookie, me);   
 router.post('/logout', verifySessionCookie, logoutUser);
 
 
