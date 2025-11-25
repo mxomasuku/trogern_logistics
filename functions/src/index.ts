275e7718bba64d1821affe534e4872d2ce5071bd
Mxolisi Masuku <mxolisi.b.masuku@gmail.com>
2025-11-25 10:20:36 +0200

    fix: incomeAggregation function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as admin from "firebase-admin";
 import {createOrUpdateVehicleServiceRecord} from "./firestore_trigger_handlers";
-import { onIncomeCreated } from "./incomeAggregration";
+import {onIncomeCreated} from "./incomeAggregration";
 
 if (!admin.apps.length) {
   admin.initializeApp();
